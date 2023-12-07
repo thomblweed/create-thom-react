9620e1b32f1b02c66e7c9f813cce3acea8d9f292
thom <thomblweed@gmail.com>
2023-12-07 17:23:08 +0000

    template update

--- templates/typescript/webpack/webpack.config.cjs
+++ templates/typescript/webpack/webpack.config.cjs
@@ -1,65 +1,65 @@
-const path = require('path');
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { EsbuildPlugin } = require("esbuild-loader");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
+const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
   mode,
   entry: {
-    index: path.resolve(__dirname, '../src/index.tsx')
+    index: path.resolve(__dirname, "../src/index.tsx"),
   },
   output: {
-    path: path.resolve(__dirname, '../public'),
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: '[name][ext]',
+    path: path.resolve(__dirname, "../public"),
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "[name][ext]",
     clean: true,
-    publicPath: '/'
+    publicPath: "/",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
     historyApiFallback: true,
     port: 1234,
     open: true,
-    hot: true
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'esbuild-loader',
+        loader: "esbuild-loader",
         options: {
-          loader: 'tsx',
-          target: 'es2015'
-        }
+          loader: "tsx",
+          target: "es2015",
+        },
       },
       {
         test: /\.s(a|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|woff)$/,
-        type: 'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx']
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      filename: 'index.html',
-      title: 'thom app',
-      inject: 'body',
-      template: path.resolve(__dirname, './template/index.html')
-    })
+      filename: "index.html",
+      title: "thom app",
+      inject: "body",
+      template: path.resolve(__dirname, "./template/index.html"),
+    }),
   ],
   optimization: {
     minimizer: [
-      new ESBuildMinifyPlugin({
-        target: 'es2015',
-        css: true
-      })
-    ]
-  }
+      new EsbuildPlugin({
+        target: "es2015",
+        css: true,
+      }),
+    ],
+  },
 };
